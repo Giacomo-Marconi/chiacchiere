89f2d9769a7a49109112bcc71c28df984c38386f
Giacomo-Marconi <19894@studenti.marconiverona.edu.it>
2024-05-07 22:18:08 +0200

    fix css, fix file, add footer

--- php/insert.php
+++ php/insert.php
@@ -1,9 +1,19 @@
 <?php
 
-error_reporting(E_ALL);
+//error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 $colors = ['CBE4F9', 'CDF5F6', 'EFF9DA', 'F9EBDF', 'F9D8D6', 'D6CDEA'];
+
+function checktesto($testo){
+    if($testo == "") return true;
+
+    $pattern = '/<[^>]*>/';
+    preg_match_all($pattern, $testo, $matches);
+    
+    return count($matches[0]) > 0 ? true : false;
+}
+
 
 require_once 'database.php';
 
@@ -12,13 +22,13 @@ $db = databaseConnection();
 if(isset($_POST['storia'])){
     echo "in<br>";
     $testo = $_POST['storia'];
-    if($testo == ""){
-        echo "vuoto";
+    
+    if(checktesto($testo)){
         $db->close();
         header('Location: ../index.html');
         exit();
     }
-    //echo $testo;
+
     $testo = str_replace("'", "\'", $testo);
 
     if(isset($_POST['colore'])){
--- script.js
+++ script.js
@@ -1,39 +1,63 @@
-const colori = ['#b25825', '#60773d', '#b8d8d8', '#d88715', '#ffe28c', '#2'];
-//              arancionino, verdognolo, azzurrino, ocra, postit, 
+const colors = ['#CBE4F9', '#CDF5F6', '#EFF9DA', '#F9EBDF', '#F9D8D6', '#F9D8D6'];
+let array = [];
 
 let p_grande;
 let sfocatura;
 let popup;
 
 
-function init() {  
+function init() {      
     fetch("http://chiacchiere.figliolo.it/php/getData.php")
         .then(response => response.json())
         .then(data => {
-            loadPost(data);
+            array = data;
+            loadPost(data.reverse());
         });
 
     p_grande = document.getElementById("post_grande");
     sfocatura= document.getElementById("sfocatura");
     popup = document.getElementById("popup");
+
+    if(!foundCookie("first")) {
+        const popIngresso = document.getElementById("popup_ingresso");
+        popIngresso.style.visibility = "visible";
+        
+        sfocatura.classList.add("sfocatura_on");
+
+        var scadenza = new Date();
+        scadenza.setDate(scadenza.getDate() + 9999); // Scadenza tra 7 giorni
+        document.cookie = "first=1; expires=" + scadenza.toUTCString();
+        //console.log(scadenza.toUTCString());
+    }
 }
+
+
+function foundCookie(name) {
+
+    const cookies = document.cookie.split(';');
+    
+    for(var i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if(cookie.indexOf(name+"=") === 0) {
+            //const valCookie = cookie.substring((name+"=").length, cookie.length);
+            //console.log("Valore del cookie: " + valCookie);
+            return true;
+        }
+    }
+    return false;
+}
+
 
 function loadPost(data) {
     for(let i=0; i<data.length; i++){
-        createPostIt(data[i].testo,i);
+        createPostIt(data[i].testo, data[i].id, data[i].color);
     }
-    setColor();
 }
 
 
-function setColor() {
-    document.querySelectorAll('.quadrato').forEach( element => {
-        element.style.backgroundColor = getRandomColor();
-    });
-}
-
-function createPostIt(testo,id) {
+function createPostIt(testo,id, color) {
     id="post_"+id;
+    console.log(id);
 
     cont = document.createElement("div");
     cont.className = "contenitore";
@@ -45,28 +69,45 @@ function createPostIt(testo,id) {
     quad.className="quadrato";
     quad.onclick = () => openPost(id);
 
-
     p=document.createElement("p");
     p.innerHTML=testo;
     p.className="testo";
     p.id=id;
+    quad.style.backgroundColor = ("#"+color);
+
+    console.log("Colore: "+color);
 
     quad.appendChild(p);
-    ombra.appendChild(quad);
+    cont.appendChild(quad);
     cont.appendChild(ombra);
 
     container = document.getElementById("container");
     container.appendChild(cont); 
 }
 
-
-
 function openPost(id) {
     console.log(id);
     p_grande.classList.add("post_grande_visibile");
     sfocatura.classList.add("sfocatura_on");
-    testo = document.getElementById(id).innerHTML;
+
+    const par = document.getElementById(id);
+    testo = par.innerHTML;
+
+    let col;
+    for (let i = 0; i < array.length; i++) {
+        if(testo==array[i].testo){
+            col=array[i].color;
+        }
+    }
+
+    console.log(col);
+    
     story = document.getElementById("story");
+
+    /*p_grande.style.backgroundColor=fromStringToColor(testo);*/
+
+    p_grande.style.backgroundColor=("#"+col);
+
     story.innerHTML = testo;
 }
 
@@ -75,34 +116,20 @@ function closePost() {
     sfocatura.classList.remove("sfocatura_on");
 }
 
-
-
-
-
 function getRandomColor() {
     return colori[Math.floor(Math.random() * colori.length)];
 }
 
-
-
-
-//--prove Luca--
-//----------------------------------------------------------------------------------------------------------------------------------
-
-
 //finestra per inserire una storia
 //-----------------------------------------------------------------------------------------------
-
-
-
 function openPopup() {
     //console.log("Cliccato");
-    popup.classList.add("openpopup");
+    cont_popup_storia.classList.add("openpopup");
     sfocatura.classList.add("sfocatura_on");
 }
 
 function closePopup() {
-    popup.classList.remove("openpopup");
+    cont_popup_storia.classList.remove("openpopup");
     sfocatura.classList.remove("sfocatura_on");
 }
 
@@ -121,6 +148,7 @@ function inviaStoria() {
 //finstra per le info
 //-----------------------------------------------------------------------------------------------
 function openInfo() {
+    closeAll();
     info.classList.add("openinfo");
     sfocatura.classList.add("sfocatura_on");
 }
@@ -131,5 +159,43 @@ function closeInfo() {
 }
 
 
+function closeAll() {
+    p_grande.classList.remove("post_grande_visibile");
+    cont_popup_storia.classList.remove("openpopup");
+    info.classList.remove("openinfo");
+    sfocatura.classList.remove("sfocatura_on");
+    popup_ingresso.classList.add("close_popup_ingresso")
+    document.getElementById("storia").value="";
+}
 
 //----------------------------------------------------------------------------------------------------------------------------------
+
+
+const fromStringToColor= (s)=>{
+    const ris = s.split("").map((ch)=>ch.charCodeAt(0));
+
+    const somma = ris.reduce((somma, ele)=>{return somma+ele}, 0);
+
+    const index=(s.length+somma)%colors.length;
+
+    return colors[index];
+};
+
+function change_color(color){
+    colore = document.getElementById("colore");
+    colore.value = color;
+    storia = document.getElementById("storia");
+    storia.style.backgroundColor = colors[color];
+    //document.querySelectorAll("scelta_colore").classList.remove("scelta_colore_on");
+    
+
+    for (let i = 0; i < 6; i++) {
+        const but = document.getElementById("butt_"+i);
+        but.classList.remove("scelta_colore_on");
+
+        if (color==i){
+            but.classList.add("scelta_colore_on");
+        }
+    }
+
+}
